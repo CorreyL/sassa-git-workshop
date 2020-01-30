cffc48d2ed85d1e91874d3d407749131401f5091
Correy Lim <correylim@gmail.com>
2020-01-29 17:14:04 -0800

    Add conditional for parsing SERIES
    The CSV from Bank of Canada is structured wherein single column titles
    in the CSV will indicate how the next few lines of the CSV will be
    structured
    
    As such, detect when the `SERIES` title is encountered, and write logic
    to parse the contents of the `SERIES` data into a couple of `dicts`.
    One which maps the three letter code of a currency to their
    corresponding `id`, and another that maps the `id` of each row to their
    corresponding `label` and `description`
    
    This will allow functions to be implemented in the future to be given a
    human readable currency three letter code (i.e. 'USD', 'AUD'), and
    retrieve the corresponding complex `id` Bank of Canada uses to identify
    conversion of a currency to CAD

--- rates.py
+++ rates.py
@@ -8,9 +8,32 @@ class Rates:
         structures necessary to get the conversion rate of different currencies
         at different times
         """
+        self.currency_to_series_id = {}
+        self.series = {}
         with open("FX_RATES_MONTHLY-sd-2017-01-01.csv") as csvfile:
             csv_contents = list(csv.reader(csvfile))
             line_idx = 0
             while line_idx < len(csv_contents):
+                line = csv_contents[line_idx]
+                # Parse the contents under the SERIES header
+                if len(line) > 0 and line[0] == "SERIES":
+                    # Start at the first row of the SERIES data
+                    line_idx = line_idx + 2
+                    line = csv_contents[line_idx]
+                    while len(line) > 0:
+                        id = line[0]
+                        label = line[1]
+                        description = line[2]
+                        self.series[id] = {
+                            "label": label,
+                            "description": description,
+                        }
+                        currency = label.split("/")[0]
+                        self.currency_to_series_id[currency] = id
+                        line_idx = line_idx + 1
+                        line = csv_contents[line_idx]
+                    # Once we are done parsing the SERIES data, continue parsing
+                    # the rest of the CSV
+                    continue
                 # Unimportant line, continue parsing the CSV
                 line_idx = line_idx + 1
