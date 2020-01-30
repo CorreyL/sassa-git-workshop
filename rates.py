bd3f81d44ea6a50cd59e59cf73337fa70dccb69d
Correy Lim <correylim@gmail.com>
2020-01-29 17:10:52 -0800

    Instantiate the class `Rates`
    Write a docstring for the constructor that indicates what the
    constructor will do

---
+++ rates.py
@@ -0,0 +1,8 @@
+class Rates:
+    def __init__(self):
+        """
+        Reads the currency conversion rate spreadsheet and builds the data
+        structures necessary to get the conversion rate of different currencies
+        at different times
+        """
+        pass
