ded724c5e731bdea95cc461eb9b1dc81c4449230
Correy Lim <correylim@gmail.com>
2020-01-31 16:50:37 -0800

    Separate core `Rates` logic from `__init__`
    Ensures that unit testing for the core logic, now defined in
    `parse_csv`, will be easily testable without having to mock an implicit
    call to `__init__`

--- rates.py
+++ rates.py
@@ -3,14 +3,17 @@ import csv
 
 class Rates:
     def __init__(self):
+        self.currency_to_series_id = {}
+        self.series = {}
+        self.observations = {}
+        self.parse_csv()
+
+    def parse_csv(self):
         """
         Reads the currency conversion rate spreadsheet and builds the data
         structures necessary to get the conversion rate of different currencies
         at different times
         """
-        self.currency_to_series_id = {}
-        self.series = {}
-        self.observations = {}
         with open("FX_RATES_MONTHLY-sd-2017-01-01.csv") as csvfile:
             csv_contents = list(csv.reader(csvfile))
             line_idx = 0
