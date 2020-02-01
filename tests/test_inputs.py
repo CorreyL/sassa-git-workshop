26a703666f9f855f0535ab11e9001399f0bed0c4
Correy Lim <correylim@gmail.com>
2020-01-31 17:28:28 -0800

    Add unit tests for getting currency from input
    Ensures that currency is validated through `Rates.check_valid_currency`,
    and the currency is returned if so, otherwise the user is prompted again
    a maximum number of times before the program exits

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -111,3 +111,73 @@ def test_get_month_input_invalid_input(
         str(captured_error_message)
         == "Number of tries exceeded. Exitting program."
     )
+
+
+@mock.patch("builtins.input")
+@mock.patch("builtins.print")
+@mock.patch("rates.Rates.get_supported_currencies")
+@mock.patch("rates.Rates.check_valid_currency")
+def test_get_currency_input_valid_input(
+    check_valid_currency,
+    get_supported_currencies,
+    builtins_print,
+    builtins_input,
+):
+    builtins_input.return_value = "AUD"
+    rates = mock.MagicMock
+    rates.check_valid_currency = check_valid_currency
+    rates.get_supported_currencies = get_supported_currencies
+    get_supported_currencies.return_value = ["AUD"]
+    captured_return = get_currency_input(rates)
+    builtins_print.assert_called_once_with(
+        f"What currency do you want to convert to CAD?\n"
+        f"Supported currencies include: {get_supported_currencies.return_value}"
+    )
+    check_valid_currency.assert_called_once_with(builtins_input.return_value)
+    assert captured_return == builtins_input.return_value
+
+
+@mock.patch("builtins.input")
+@mock.patch("builtins.print")
+@mock.patch("rates.Rates.get_supported_currencies")
+@mock.patch("rates.Rates.check_valid_currency")
+def test_get_currency_input_invalid_input(
+    check_valid_currency,
+    get_supported_currencies,
+    builtins_print,
+    builtins_input,
+):
+    builtins_input.return_value = "JPY"
+    rates = mock.MagicMock
+    rates.get_supported_currencies = get_supported_currencies
+    rates.check_valid_currency = check_valid_currency
+    error_message = "errmsg"
+    check_valid_currency.side_effect = Exception(error_message)
+    get_supported_currencies.return_value = ["AUD"]
+    captured_error_message = None
+    try:
+        get_currency_input(rates)
+    except Exception as err:
+        captured_error_message = err
+
+    builtins_print.assert_has_calls(
+        [
+            mock.call(
+                f"What currency do you want to convert to CAD?\n"
+                "Supported currencies include: "
+                f"{get_supported_currencies.return_value}"
+            ),
+            mock.call(f"{error_message}\n",),
+        ],
+    )
+    check_valid_currency.assert_has_calls(
+        [
+            mock.call(builtins_input.return_value),
+            mock.call(builtins_input.return_value),
+            mock.call(builtins_input.return_value),
+        ]
+    )
+    assert (
+        str(captured_error_message)
+        == "Number of tries exceeded. Exitting program."
+    )
