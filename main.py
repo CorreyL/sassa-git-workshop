55fec5d9e0db4d4937eaa2cc41707e07909964e4
Correy Lim <correylim@gmail.com>
2020-01-31 17:01:39 -0800

    Add entry-point script for the codebase
    - Call `inputs.get_args` in preparation for getting user input for
    `year`, `month`, and `currency`
    - Instantiate an instance of `Rates` to parse the data from the CSV file
    - Define a `main` function, and only invoke it if `main.py` is the
    entry-point used, ensuring invocation of other scripts won't erroneously
    cause `main` to execute
    - Assign `year`, `month` and `currency` from the argparse, otherwise
    call their respective `input` invoking functions, ensuring a call to
    `Rates.get_rate` will always have valid inputs
    - Print the results of `get_rate` with valid arguments, ensuring the
    user receives the information they started the program for

---
+++ main.py
@@ -0,0 +1,17 @@
+from argparser import get_args
+from inputs import get_year_input, get_month_input, get_currency_input
+from rates import Rates
+
+args = get_args()
+rates = Rates()
+
+def main():
+    print("Welcome to the Historical Rate Calculator")
+    year = args.year if args.year else get_year_input(rates)
+    month = args.month if args.month else get_month_input(rates)
+    currency = args.currency if args.currency else get_currency_input(rates)
+    conversion_rate = rates.get_rate(year, month, currency)
+    print(f"Conversion rate of {currency} to CAD: {conversion_rate}")
+
+if __name__ == "__main__":
+    main()
